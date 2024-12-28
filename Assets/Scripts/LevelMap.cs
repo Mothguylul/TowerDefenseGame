30b00159f6947cad6679cca311e863f478801cea
mothguylul <juliusmuench89gmail.com>
2024-12-28 19:04:15 +0100

    Cleaned SpawnEnemys.cs and LevelMap.cs up, shortend code, works fine

---
+++ Assets/Scripts/LevelMap.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.Logic;
+using Unity.Burst.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.WSA;
+
+/// <summary>
+/// Looks at the Level it gets and creates the visuals for it
+/// </summary>
+public class LevelMap : MonoBehaviour
+{
+	public Map Map;
+
+	private Grid _grid;
+
+	private Vector3 startpointCords;
+	public Vector2 StartPoint => _grid.LocalToWorld(startpointCords);
+
+	public Tilemap TileMap;
+
+	private Camera _camera;
+
+	private Vector3 centerPoint;
+
+	private MapTile[,] MapTiles;
+
+	[SerializeField] public Level Level;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		MapTiles = Level.GetMapTiles();
+		Map = new Map(MapTiles);
+		startpointCords = new Vector3(Map.StartPoint.X, -Map.StartPoint.Y, 0);
+		TileMap = GetComponent<Tilemap>();
+		PaintTiles();
+		centerPoint = CalculateCenterPoint();
+		_grid = FindObjectOfType<Grid>();
+		_camera = Camera.main;
+		_camera.transform.position = centerPoint;
+
+	}
+
+	private void PaintTiles()
+	{
+		for (int y = 0; y < Map.Height; y++)
+		{
+			for (int x = 0; x < Map.Width; x++)
+			{
+				MapTile currentMaptile = Map.GetBoardContents(y, x);
+				TileBase tileToPlace = currentMaptile.IsWalkable
+					? Level.GetRandomWalkableTile()
+					: Level.BuildableTile;
+				TileMap.SetTile(new Vector3Int(x, -y), tileToPlace);
+			}
+		}
+		TileMap.RefreshAllTiles();
+	}
+
+	private Vector3 CalculateCenterPoint()
+	{
+		BoundsInt bounds = TileMap.cellBounds;
+
+		Vector3 min = TileMap.CellToWorld(bounds.min);
+		Vector3 max = TileMap.CellToWorld(bounds.max);
+
+		Vector3 center = (min + max) / 2;
+		center.z = -10;
+
+		return center;
+	}
+}
--- Assets/Scripts/Logic/SpawnEnemy.cs
+++ Assets/Scripts/Logic/SpawnEnemy.cs
@@ -11,31 +11,14 @@ public class SpawnEnemy : MonoBehaviour
 {
 	[SerializeField] private GameObject _enemyPrefab;
 
-	private Vector3 _startpoint = new Vector3();
-
-	private MapTile[,] mapTiles;
-
-	private int boardheight, boardwidth;
-
 	private LevelMap _levelMap;
-
-	private Map _map;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
 		_levelMap = FindObjectOfType<LevelMap>();
-		_map = _levelMap.Map;
-		boardheight = _map.Height;
-		boardwidth = _map.Width;
-		mapTiles = new MapTile[boardwidth, boardheight];
-		mapTiles = _levelMap.Level.GetMapTiles();
-		_startpoint = FindStartPointInTilemap();
-
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 
@@ -43,8 +26,7 @@ public class SpawnEnemy : MonoBehaviour
 		{
 			if (_enemyPrefab != null)
 			{
-				Console.WriteLine($"enemyprefab position: {_enemyPrefab.transform.position} startpoint: {_startpoint.y}{_startpoint.x} ");
-				Instantiate(_enemyPrefab, _startpoint, Quaternion.identity);
+				Instantiate(_enemyPrefab, _levelMap.StartPoint, Quaternion.identity);
 			}
 			else
 			{
@@ -52,27 +34,5 @@ public class SpawnEnemy : MonoBehaviour
 			}
 		}
 
-	}
-
-	/// <summary>
-	/// Finds the maptile which is the startpoint and returns it
-	/// </summary>
-	/// <returns></returns>
-	/// <exception cref="Exception"></exception>
-	private Vector3 FindStartPointInTilemap()
-	{
-		for (int x = 0; x < mapTiles.GetLength(0); x++)
-		{
-			for (int y = 0; y < mapTiles.GetLength(1); y++)
-			{
-				if (mapTiles[x, y].IsStartPoint)
-				{
-					Vector3Int cellPosition = new Vector3Int(x, -y, 0);
-					return _levelMap.TileMap.CellToWorld(cellPosition);
-				}
-			}
-		}
-
-		throw new Exception("Start point not found in map tiles.");
 	}
 }
