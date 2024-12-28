f8abdd0bb40cdd870c44a7d59604a4477c70d197
mothguylul <juliusmuench89gmail.com>
2024-12-28 18:10:38 +0100

    Added LevelMap ref, overworked FindStartPointInTileMap() loops, works fine

--- Assets/Scripts/Logic/SpawnEnemy.cs
+++ Assets/Scripts/Logic/SpawnEnemy.cs
@@ -5,42 +5,46 @@ using System.Collections.Generic;
 using TowerDefense.Logic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
 
 public class SpawnEnemy : MonoBehaviour
 {
 	[SerializeField] private GameObject _enemyPrefab;
-	private Tilemap _tilemap;
+
+	private Vector3 _startpoint = new Vector3();
+
+	private MapTile[,] mapTiles;
+
+	private int boardheight, boardwidth;
+
+	private LevelMap _levelMap;
 
 	private Map _map;
-
-	private Level _level;
-
-	private int _boardHeight, _boardWidth;
-
-	private Vector3 _startpoint;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		MapTile[,] maptiles = _level.GetMapTiles();
-		_tilemap = GetComponent<Tilemap>();
-		_boardHeight = _map.Height;
-		_boardWidth = _map.Width;
-		 _startpoint = FindStartPointInTilemap();
+
+		_levelMap = FindObjectOfType<LevelMap>();
+		_map = _levelMap.Map;
+		boardheight = _map.Height;
+		boardwidth = _map.Width;
+		mapTiles = new MapTile[boardwidth, boardheight];
+		mapTiles = _levelMap.Level.GetMapTiles();
+		_startpoint = FindStartPointInTilemap();
 
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		Console.WriteLine($"enemyprefab position: {_enemyPrefab.transform.position} startpoint: {_startpoint.y}{_startpoint.x} ");
 
-		if (Input.GetKeyDown(KeyCode.E))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			if (_enemyPrefab != null)
 			{
+				Console.WriteLine($"enemyprefab position: {_enemyPrefab.transform.position} startpoint: {_startpoint.y}{_startpoint.x} ");
 				Instantiate(_enemyPrefab, _startpoint, Quaternion.identity);
-				Console.WriteLine("Trying to set enemy to startpos");
 			}
 			else
 			{
@@ -57,25 +61,18 @@ public class SpawnEnemy : MonoBehaviour
 	/// <exception cref="Exception"></exception>
 	private Vector3 FindStartPointInTilemap()
 	{
-		BoundsInt bounds = _tilemap.cellBounds;
-
-		for (int x = bounds.xMin; x < bounds.xMax; x++)
+		for (int x = 0; x < mapTiles.GetLength(0); x++)
 		{
-			for (int y = bounds.yMin; y < bounds.yMax; y++)
+			for (int y = 0; y < mapTiles.GetLength(1); y++)
 			{
-				Vector3Int cellPosition = new Vector3Int(x, y, 0);
-
-				MapTile maptile = _map.GetBoardContents(x, y);
-
-				TileBase tile = _tilemap.GetTile(cellPosition);
-				if (tile != null && maptile.IsStartPoint)
+				if (mapTiles[x, y].IsStartPoint)
 				{
-					Vector3 worldPosition = _tilemap.CellToWorld(cellPosition);
-					return worldPosition;
+					Vector3Int cellPosition = new Vector3Int(x, -y, 0);
+					return _levelMap.TileMap.CellToWorld(cellPosition);
 				}
 			}
 		}
 
-		throw new Exception("No start point tile found in the tilemap.");
+		throw new Exception("Start point not found in map tiles.");
 	}
 }
